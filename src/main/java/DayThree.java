44497e437580a826c33536b8bb7fcfd8899c6569
Melina Cirverius <mc222ap@student.lnu.se>
2020-12-03 12:27:55 +0100

    Adding solution to part two day three

--- src/main/java/DayThree.java
+++ src/main/java/DayThree.java
@@ -10,46 +10,67 @@ import java.util.Scanner;
  */
 public class DayThree {
 
-    private int treeCounter = 0;
     private int position = 0;
+    private int counter = 0;
 
     public void solveDayThree()
     {
+        double treeCounterR1D1 = readSlope(1, 0);
+        double treeCounterR3D1 = readSlope(3, 0);
+        double treeCounterR5D1 = readSlope(5, 0);
+        double treeCounterR7D1 = readSlope(7, 0);
+        double treeCounterR1D2 = readSlope(1, 1);
+
+        System.out.println("---RESULTS---");
+        System.out.println("Number of trees encountered with right 1, down 1: " + treeCounterR1D1);
+        System.out.println("Number of trees encountered with right 3, down 1: " + treeCounterR3D1);
+        System.out.println("Number of trees encountered with right 5, down 1: " + treeCounterR5D1);
+        System.out.println("Number of trees encountered with right 7, down 1: " + treeCounterR7D1);
+        System.out.println("Number of trees encountered with right 1, down 2: " + treeCounterR1D2);
+        System.out.printf("Trees multiplied: %.0f\n", treeCounterR1D1 * treeCounterR3D1 * treeCounterR5D1 *
+                treeCounterR7D1 * treeCounterR1D2);
+    }
+
+    private int readSlope(final int increase, final int row)
+    {
         File file = new File("src/main/resources/trajectory.txt");
+        counter = 0;
+        position = 0;
 
         try
         {
             Scanner scanner = new Scanner(file);
-            readSlope(scanner);
+
+            int lineWidth = scanner.nextLine().length(); // first line
+
+            while (scanner.hasNext())
+            {
+                position = position + increase;
+
+                if (row > 0) // For instances where the move is down two.
+                {
+                    scanner.nextLine();
+                }
+
+                if (position < lineWidth)
+                {
+                    checkForTree(scanner);
+                }
+                else
+                {
+                    position = position - 31;
+
+                    checkForTree(scanner);
+                }
+            }
+            scanner.close();
         }
         catch (FileNotFoundException e)
         {
             System.out.println("File not found!");
         }
 
-        System.out.println("---RESULTS---");
-        System.out.println("Number of trees encountered: " + treeCounter);
-    }
-
-    private void readSlope(final Scanner scanner)
-    {
-        int lineWidth = scanner.nextLine().length(); // first line
-
-        while (scanner.hasNext())
-        {
-            position = position + 3;
-
-            if (position < lineWidth)
-            {
-                checkForTree(scanner);
-            }
-            else
-            {
-                position = position - 31;
-
-                checkForTree(scanner);
-            }
-        }
+        return counter;
     }
 
     private void checkForTree(Scanner scanner)
@@ -58,7 +79,7 @@ public class DayThree {
 
         if (currentLine.charAt(position) == '#')
         {
-            treeCounter++;
+            counter++;
         }
     }
 }

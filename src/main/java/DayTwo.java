efb967b30fd5f0c397e07f8bac6aa503df3575ca
Melina Cirverius <mc222ap@student.lnu.se>
2020-12-03 08:14:24 +0100

    Adding solution for day two

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -11,10 +11,12 @@ import java.util.Scanner;
  */
 public class DayTwo {
 
-    private String minMax;
-    private String letter;
-    private String password;
+    private int firstValue = 0;
+    private int secondValue = 0;
+    private char letterToTest;
+    private ArrayList<Character> passwordChars;
     private int validCounter = 0;
+    private int validCounterTwo = 0;
 
     public void solveDayTwo()
     {
@@ -32,36 +34,44 @@ public class DayTwo {
 
         System.out.println("--- RESULTS---");
         System.out.println("Number of valid passwords according to first puzzle policy: " + validCounter);
+        System.out.println("Number of valid passwords according to second puzzle policy: " + validCounterTwo);
     }
 
     private void readPasswords(final Scanner scanner)
     {
         while (scanner.hasNext())
         {
-            minMax = scanner.next(); // The first two numbers.
-            letter = scanner.next(); // The letter that is needed.
-            password = scanner.nextLine(); // The given password.
+            String minMax = scanner.next(); // The first two numbers.
+            String letter = scanner.next(); // The letter that is needed.
+            String password = scanner.nextLine(); // The given password.
 
+            formatInputs(minMax, letter, password);
             checkPassword();
+            checkPasswordAgain();
         }
     }
 
-    private void checkPassword()
+    private void formatInputs(final String minMax, final String letter, String password)
     {
         String[] minMaxValue = minMax.split("-", 2);
 
-        int minValue = Integer.parseInt(minMaxValue[0]);
-        int maxValue = Integer.parseInt(minMaxValue[1]);
+        firstValue = Integer.parseInt(minMaxValue[0]);
+        secondValue = Integer.parseInt(minMaxValue[1]);
 
-        char letterToTest = letter.charAt(0);
-        ArrayList<Character> passwordChars = new ArrayList<Character>();
+        letterToTest = letter.charAt(0);
 
-        int counter = 0;
+        passwordChars = new ArrayList<Character>();
+        password = password.replaceAll("\\s", "");
 
         for (int i = 0; i < password.length(); i++)
         {
             passwordChars.add(password.charAt(i));
         }
+    }
+
+    private void checkPassword()
+    {
+        int counter = 0;
 
         for (char character : passwordChars)
         {
@@ -71,9 +81,32 @@ public class DayTwo {
             }
         }
 
-        if (counter >= minValue && counter <= maxValue)
+        if (counter >= firstValue && counter <= secondValue)
         {
             validCounter++;
+        }
+    }
+
+    private void checkPasswordAgain()
+    {
+        int occurrences = 0;
+
+        if (passwordChars.size() >= secondValue) {
+
+            if (letterToTest == passwordChars.get(firstValue - 1))
+            {
+                occurrences++;
+            }
+
+            if (letterToTest == passwordChars.get(secondValue - 1))
+            {
+                occurrences++;
+            }
+        }
+
+        if (occurrences == 1)
+        {
+            validCounterTwo++;
         }
     }
 

f4e86cc70a11f72a88ccbb1469ec86e83174683f
Melina Cirverius <mc222ap@student.lnu.se>
2020-12-10 16:07:30 +0100

    Adding solution to part two day nine

--- src/main/java/DayNine.java
+++ src/main/java/DayNine.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,7 +16,10 @@ import java.util.List;
 public class DayNine extends December {
 
     private List<Long> numbers = new ArrayList<>();
+    private List<Long> countingNumbers = new ArrayList<>();
     private int next = 25;
+    private long invalidNum = 0;
+    private boolean correctSum = false;
 
     void setUp(String inputFile) {
 
@@ -38,8 +42,8 @@ public class DayNine extends December {
         while (checkNextNum()) {
             next++;
         }
-
-        System.out.println("Firt invalid number is: " + numbers.get(next));
+        invalidNum = numbers.get(next);
+        System.out.println("First invalid number is: " + invalidNum);
     }
 
     private boolean checkNextNum() {
@@ -66,5 +70,49 @@ public class DayNine extends December {
 
     void part2() {
 
+        for (Long num : numbers) {
+
+            if (!correctSum) {
+                countingNumbers.clear();
+                countNumbers(num);
+            }
+        }
+
+        findWeakness();
+    }
+
+    private void countNumbers(Long num) {
+
+        if (num < invalidNum) {
+
+            countingNumbers.add(num);
+            int position = numbers.indexOf(num);
+
+            for (int i = position + 1; i < numbers.size(); i++) {
+
+                long current = numbers.get(i);
+                countingNumbers.add(current);
+
+                num += current;
+
+                if (num == invalidNum) {
+                    correctSum = true;
+                    break;
+                }
+                else if (num > invalidNum) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private void findWeakness() {
+
+        countingNumbers.sort(null);
+
+        Long smallest = countingNumbers.get(0);
+        Long largest = countingNumbers.get(countingNumbers.size() -1);
+
+        System.out.println("Encryption weakness is: " + (smallest + largest));
     }
 }
